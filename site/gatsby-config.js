c20325efb00b9f8e5f2714b0e8110155f1a1d2cd
Magne Rasmussen <magne.rasmussen@gmail.com>
2020-03-08 01:21:12 +0100

    Add Google Analytics.

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,8 +1,14 @@
 module.exports = {
   plugins: [
     {
-      resolve: "gatsby-theme-codehive",
+      resolve: 'gatsby-theme-codehive',
       options: {}
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: `${process.env.GA_TRACKING_ID}`
+      }
     }
   ]
 }
