645a61da7c8d6326643d762e6c8b9b5df08e24e2
Magne Rasmussen <magne.rasmussen@gmail.com>
2020-03-09 07:33:35 +0100

    Gatsby tutorial 5. Source Plugins and Rendering Queried Data

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -14,6 +14,14 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
