53eaea9afb3019ab9d21523e36474d6f1d2e2330
Magne Rasmussen <magne.rasmussen@gmail.com>
2020-03-15 10:18:12 +0100

    Typescript integration.

--- theme/src/pages/index.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export default () => <h1>My Site!</h1>;
---
+++ theme/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const header = (): JSX.Element => <h1>My Site!</h1>;
+
+export default header;
