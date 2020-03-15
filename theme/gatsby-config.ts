61c00c34436798f9f70fd43498904c10a94ac01c
Magne Rasmussen <magne.rasmussen@gmail.com>
2020-03-15 11:10:44 +0100

    Add codegen and styled-components.

--- theme/gatsby-config.ts
+++ theme/gatsby-config.ts
@@ -1,5 +1,18 @@
 export default ({ contentPath = 'data' }): {} => ({
-  plugins: ['gatsby-plugin-typescript'],
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-codegen',
+      options: {
+        addTypename: true,
+        includes: [
+          './node_modules/theme/src/**/*.ts',
+          './node_modules/theme/src/**/*.tsx',
+          '../node_modules/theme/src/**/*.ts',
+          '../node_modules/theme/src/**/*.tsx',
+        ],
+      },
+    },
+  ],
 });
-
-interface Test {}
