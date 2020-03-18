db75028330d2937cb99e136596f22473be9a9154
Magne Rasmussen <magne.rasmussen@gmail.com>
2020-03-18 14:04:36 +0100

    Add page template.

--- theme/src/templates/page.tsx
+++ theme/src/templates/page.tsx
@@ -1,4 +1,11 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import { Container } from '../components/common';
+import Layout from '../components/layout';
+import PageSidebarContent from '../components/page-sidebar-content';
+import SEO from '../components/seo';
+import Subheader from '../components/subheader';
+import Theme from '../styles/theme';
 import { IPage } from '../utils/models';
 
 interface IPageTemplateProps {
@@ -7,8 +14,48 @@ interface IPageTemplateProps {
   };
   location: Location;
 }
+
+const PageContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+
+  @media (max-width: ${Theme.breakpoints.md}) {
+    display: block;
+  }
+
+  p:first-child {
+    margin-top: 0;
+  }
+
+  li > a,
+  p > a  {
+    color: ${Theme.layout.linkColor};
+    border-bottom: 2px ${Theme.layout.linkColor} solid;
+  }
+`;
+
+const PageSidebar = styled.aside`
+  margin-left: 50px;
+  @media (max-width: ${Theme.breakpoints.md}) {
+    margin-left: 0;
+  }
+`;
+
 const PageTemplate: FunctionComponent<IPageTemplateProps> = ({ pathContext, location }) => {
-  return <div />;
+  const page = pathContext.page;
+
+  return (
+    <Layout bigHeader={false}>
+      <SEO title={page.frontmatter.title} description={page.frontmatter.excerpt} location={location} />
+      <Subheader title={page.frontmatter.title} backgroundColor={Theme.layout.primaryColor} />
+      <PageContainer>
+        <section dangerouslySetInnerHTML={{ __html: page.html }} />
+        <PageSidebar>
+          <PageSidebarContent />
+        </PageSidebar>
+      </PageContainer>
+    </Layout>
+  );
 };
 
 export default PageTemplate;
