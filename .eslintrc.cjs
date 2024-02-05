fd38e57afeec37d52352a6de2e4afbf6456246b5
Magne Rasmussen <magne.rasmussen@gmail.com>
2024-02-05 07:50:46 +0100

    Initial commit.
    Astro skeleton with TailwindCSS and MDX integrations. Working
    Typescript, ESLint, and Prettier configuration.

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:astro/recommended',
+    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest', // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    tsconfigRootDir: __dirname
+  },
+  rules: {
+    // Place to specify ESLint rules.
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        'astro/no-set-html-directive': 'error'
+      }
+    }
+  ]
+}
---
+++ prettier.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+
+  plugins: [require.resolve('prettier-plugin-astro'), require.resolve('prettier-plugin-tailwindcss')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+        tabWidth: 2
+      }
+    }
+  ]
+}
