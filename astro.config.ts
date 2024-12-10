1f44b8d99fae520373a95a72f9b066060cacc091
Magne Rasmussen <magne.rasmussen@gmail.com>
2024-12-10 12:15:59 +0100

    Upgrade to Astro 5.

--- astro.config.ts
+++ astro.config.ts
@@ -33,5 +33,8 @@ export default defineConfig({
     syntaxHighlight: false,
     remarkPlugins: [remarkReadingTime, remarkMath],
     rehypePlugins: [rehypeMathjax, [rehypePrettyCode, prettyCodeOptions]]
+  },
+  legacy: {
+    collections: true
   }
 })
